b5a880e42dbaa719e5bf1f3d6d3e6c95f954a93a
Breena Greek <bgreek2019@gmail.com>
2020-01-29 21:28:43 -0500

    Initial Commit

---
+++ Swift-Vocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Vocabulary
+//
+//  Created by Breena Greek on 1/29/20.
+//  Copyright © 2020 Breena Greek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
